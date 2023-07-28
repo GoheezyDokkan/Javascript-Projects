2c7fd49829e25c8c9b2dc12842cb1a84d279051c
GoheezyDokkan <134324302+GoheezyDokkan@users.noreply.github.com>
2023-07-27 21:59:49 -0400

    Add files via upload

---
+++ Project1_expressionless_alert/JS/script.js
@@ -0,0 +1,11 @@
+window.alert("Hello, instructor. I hope you're having a great day! :D"); // This is a window alert
+
+var Sent1 = "This is the beginning of the string"; // Define variable 1
+var Sent2 = " and this is the end of the string"; // Define variable 2
+var ConcatenateStrings = Sent1 + Sent2; // Concatenate the two strings
+document.write(ConcatenateStrings); // Write the result to the console
+
+expression1 = 3 + 3; // This is an expression
+
+document.write("<br>" + expression1); // This is a statement that writes the 
+                             // result of the previously defined expression
